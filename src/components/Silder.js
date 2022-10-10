41ed027bc2f5608175be98ee22f0cdf54ff2210e
Vinay <vinaynn123@gmail.com>
2022-10-10 17:22:57 +0530

    resolved errors

--- src/components/Silder.js
+++ src/components/Silder.js
@@ -1,45 +1,41 @@
 import React from "react";
-import "./Slider.css"
-import { useInView } from 'react-intersection-observer';
+import "./Slider.css";
+import { useInView } from "react-intersection-observer";
 
+const Slider = ({ imageSrc, title, subtitle, flipped }) => {
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
 
-const Slider = ({ imageSrc, title, subtitle, flipped }) =>{
-    const { ref, inView, entry } = useInView({
-        /* Optional options */
-        threshold: 0.5
-      });
-    
-
-const renderContent = () =>{
-    if (!flipped){
-        return (
-            <>
-            <img src={imageSrc} alt= "travel" className="slider_image"/>
-            <div className="slider_content">
-                <h1 className="slider_title">{title}</h1>
-                <p className="slider_subtile">{subtitle}</p>
-            </div>
-            </>
-        )
+  const renderContent = () => {
+    if (!flipped) {
+      return (
+        <>
+          <img src={imageSrc} alt="travel" className="slider_image" />
+          <div className="slider_content">
+            <h1 className="slider_title">{title}</h1>
+            <p className="slider_subtile">{subtitle}</p>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="slider_content">
+            <h1 className="slider_title">{title}</h1>
+            <p className="slider_subtile">{subtitle}</p>
+          </div>
+          <img src={imageSrc} alt="travel" className="slider_image" />
+        </>
+      );
     }
-    else{
-        return (
-            <>
-            <div className="slider_content">
-                <h1 className="slider_title">{title}</h1>
-                <p className="slider_subtile">{subtitle}</p>
-            </div>
-            <img src={imageSrc} alt= "travel" className="slider_image"/>
-            </>
-        )
-    }
-    
-}
+  };
 
-    return (
-        <div className={ inView ? "slider slider_zoom": "slider"} ref= {ref}>
-            {renderContent()}
-        </div>
-    )
-} 
+  return (
+    <div className={inView ? "slider slider_zoom" : "slider"} ref={ref}>
+      {renderContent()}
+    </div>
+  );
+};
 export default Slider;
